4f3b8cfe7904bc638d0f75f81c34a4c89a8d6669
Zeerak Zubair <zeerakzubair047@gmail.com>
2023-09-19 21:30:46 +0500

    Added MLP class file

---
+++ mlp.py
@@ -0,0 +1,7 @@
+class MLP:
+    def __init__(self, architecture):
+        self.architecture = architecture
+        self.weight = []
+        self.biases = []
+        self.activations = []
+        
