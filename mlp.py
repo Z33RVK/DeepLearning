20bc9a931242d62b82a899bdd7d9396ac15d2a82
Zeerak Zubair <zeerakzubair047@gmail.com>
2023-09-28 19:12:10 +0500

    Updated code files mlp.py and task1.py

--- mlp.py
+++ mlp.py
@@ -1,101 +1,108 @@
 import numpy as np
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import hamming_loss
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.preprocessing import label_binarize
 
-class MLP:
-    def __init__(self, hidden_layers=(100,), activation='relu', learning_rate=0.01, momentum=0.9, epochs=100):
+class MLPClassifier:
+    def __init__(self, input_size, hidden_layers, output_size):
+        self.input_size = input_size
         self.hidden_layers = hidden_layers
-        self.activation = activation
-        self.learning_rate = learning_rate
-        self.momentum = momentum
-        self.epochs = epochs
+        self.output_size = output_size
         self.weights = []
-    
+        self.biases = []
+
+        # Initialize weights and biases for each layer
+        layer_sizes = [input_size] + hidden_layers + [output_size]
+        for i in range(len(layer_sizes) - 1):
+            weight_shape = (layer_sizes[i], layer_sizes[i+1])
+            bias_shape = (1, layer_sizes[i+1])
+            self.weights.append(np.random.randn(*weight_shape))
+            self.biases.append(np.random.randn(*bias_shape))
+
+    def forward_propagation(self, X):
+        activations = [X]
+        for i in range(len(self.weights)):
+            weighted_sum = np.dot(activations[i], self.weights[i]) + self.biases[i]
+            activation = self.sigmoid(weighted_sum)
+            activations.append(activation)
+        return activations
+
+    def backward_propagation(self, X, y, activations, learning_rate):
+        deltas = [(activations[-1] - y) * self.sigmoid_derivative(activations[-1])]
+        for i in range(len(self.weights) - 1, 0, -1):
+            delta = np.dot(deltas[0], self.weights[i].T) * self.sigmoid_derivative(activations[i])
+            deltas.insert(0, delta)
+
+        for i in range(len(self.weights)):
+            self.weights[i] -= learning_rate * np.dot(activations[i].T, deltas[i])
+            self.biases[i] -= learning_rate * np.sum(deltas[i], axis=0)
+
+    def fit(self, X_train, y_train, learning_rate=0.1, epochs=1000):
+        for epoch in range(epochs):
+            X_train, y_train = shuffle(X_train, y_train)
+            activations = self.forward_propagation(X_train)
+            self.backward_propagation(X_train, y_train, activations, learning_rate)
+
+    def predict(self, X):
+        activations = self.forward_propagation(X)
+        predictions = np.argmax(activations[-1], axis=1)
+        return predictions
+
+    def test(self, X_test, y_test):
+        predictions = self.predict(X_test)
+
+        if len(y_test.shape) == 1 or y_test.shape[1] == 1:
+            # Multiclass classification
+            multiclass_accuracy = accuracy_score(y_test, predictions)
+            multilabel_accuracy = None
+        else:
+            # Multilabel classification
+            binarized_y_test = label_binarize(y_test, classes=list(range(self.output_size)))
+            multilabel_accuracy = hamming_loss(binarized_y_test, predictions)
+            multiclass_accuracy = None
+
+        return multiclass_accuracy, multilabel_accuracy
+
+    def tune(self, X_train, y_train, param_grid, cv=5):
+        # Perform grid search to find the best hyperparameters
+        best_accuracy = 0
+        best_params = {}
+
+        for hidden_layers in param_grid['hidden_layers']:
+            for learning_rate in param_grid['learning_rate']:
+                for epochs in param_grid['epochs']:
+                    mlp = MLPClassifier(input_size=self.input_size, hidden_layers=hidden_layers,
+                                        output_size=self.output_size)
+                    mlp.fit(X_train, y_train, learning_rate=learning_rate, epochs=epochs)
+                    accuracy = mlp.test(X_train, y_train)
+
+                    if accuracy > best_accuracy:
+                        best_accuracy = accuracy
+                        best_params = {
+                            'hidden_layers': hidden_layers,
+                            'learning_rate': learning_rate,
+                            'epochs': epochs
+                        }
+
+        self.hidden_layers = best_params['hidden_layers']
+        self.learning_rate = best_params['learning_rate']
+        self.epochs = best_params['epochs']
+
     def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-    
+        # Clip the values to prevent overflow or underflow
+        clipped_x = np.clip(x, -500, 500)
+        return 1 / (1 + np.exp(-clipped_x))
+
     def sigmoid_derivative(self, x):
         return x * (1 - x)
-    
-    def relu(self, x):
-        return np.maximum(0, x)
-    
-    def relu_derivative(self, x):
-        x[x <= 0] = 0
-        x[x > 0] = 1
-        return x
-    
-    def initialize_weights(self, input_dim, output_dim):
-        self.weights.append(np.random.randn(input_dim, self.hidden_layers[0]))
-        for i in range(len(self.hidden_layers) - 1):
-            self.weights.append(np.random.randn(self.hidden_layers[i], self.hidden_layers[i+1]))
-        self.weights.append(np.random.randn(self.hidden_layers[-1], output_dim))
-    
-    def forward_propagation(self, X):
-        self.layers = []
-        input_data = X
-        for i in range(len(self.weights)):
-            self.layers.append(input_data)
-            net_input = np.dot(input_data, self.weights[i])
-            if self.activation == 'sigmoid':
-                input_data = self.sigmoid(net_input)
-            elif self.activation == 'relu':
-                input_data = self.relu(net_input)
-        self.layers.append(input_data)
-    
-    def backward_propagation(self, X, y):
-        error = y - self.layers[-1]
-        deltas = [error * self.sigmoid_derivative(self.layers[-1])]
-        
-        for i in range(len(self.layers) - 2, 0, -1):
-            error = deltas[-1].dot(self.weights[i].T)
-            if self.activation == 'sigmoid':
-                delta = error * self.sigmoid_derivative(self.layers[i])
-            elif self.activation == 'relu':
-                delta = error * self.relu_derivative(self.layers[i])
-            deltas.append(delta)
-        
-        for i in range(len(self.weights) - 1, -1, -1):
-            self.weights[i] += self.learning_rate * self.layers[i].T.dot(deltas.pop())
-    
-    def fit(self, X, y):
-        self.initialize_weights(X.shape[1], y.shape[1])
-        self.train_accuracies = []
-        self.valid_accuracies = []
-        
-        for epoch in range(self.epochs):
-            self.forward_propagation(X)
-            self.backward_propagation(X, y)
-            
-            train_pred = self.predict(X)
-            train_acc = accuracy_score(np.argmax(y, axis=1), np.argmax(train_pred, axis=1))
-            self.train_accuracies.append(train_acc)
-            
-            valid_pred = self.predict(X_valid)
-            valid_acc = accuracy_score(np.argmax(y_valid, axis=1), np.argmax(valid_pred, axis=1))
-            self.valid_accuracies.append(valid_acc)
-            
-            print(f"Epoch {epoch+1}/{self.epochs} - Train Accuracy: {train_acc:.4f} - Validation Accuracy: {valid_acc:.4f}")
-        
-    def predict(self, X):
-        self.forward_propagation(X)
-        return self.layers[-1]
-    
-    def test(self, X, y):
-        y_pred = self.predict(X)
-        acc = accuracy_score(np.argmax(y, axis=1), np.argmax(y_pred, axis=1))
-        precision = precision_score(np.argmax(y, axis=1), np.argmax(y_pred, axis=1), average='weighted')
-        recall = recall_score(np.argmax(y, axis=1), np.argmax(y_pred, axis=1), average='weighted')
-        f1 = f1_score(np.argmax(y, axis=1), np.argmax(y_pred, axis=1), average='weighted')
-        confusion = confusion_matrix(np.argmax(y, axis=1), np.argmax(y_pred, axis=1))
-        
-        print(f"Test Accuracy: {acc:.4f}")
-        print(f"Precision: {precision:.4f}")
-        print(f"Recall: {recall:.4f}")
-        print(f"F1-score: {f1:.4f}")
-        print("Confusion Matrix:")
-        print(confusion)
-    
-    def tune(self, X, y, param_grid):
-        model = GridSearchCV(estimator=self, param_grid=param_grid, cv=5, scoring='accuracy')
--- task1.py
+++ task1.py
@@ -1,19 +1,158 @@
 from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split, GridSearchCV
+import numpy as np
+from mlp import MLPClassifier
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-#path to dataset
+def analyze_input_data(X):
+    data_format = None
+    data_type = None
+    
+    if isinstance(X, np.ndarray):
+        data_format = "numpy array"
+        data_type = X.dtype
+    elif isinstance(X, pd.DataFrame):
+        data_format = "Pandas DataFrame"
+        data_type = X.dtypes
+    elif isinstance(X, list):
+        data_format = "list"
+        data_type = type(X[0])
+    elif isinstance(X, str):
+        data_format = "string"
+        data_type = "Not applicable"
+    else:
+        data_format = "unknown"
+        data_type = "unknown"
+    
+    return data_format, data_type
+
+ #path to dataset
+
+# Define a function to extract the person label from the file name
+def extract_person_label(file_name):
+    return int(file_name.split('.')[0].replace('subject', '')) - 1  # Subtract 1 to make labels start from 0
+
 yale_dataset_path = "./archive" 
 
-# loading the Yale image dataset
+# # loading the Yale image dataset
 yale_dataset = load_files(yale_dataset_path, shuffle=False)
 
-# Access the data and target labels
-X = yale_dataset.data  # Data (images)
-Y = yale_dataset.target  # Target labels
+data = []  # List to store image data
+labels = []  # List to store labels
 
-##print(X)
-##print(Y)
+for file_name in os.listdir(yale_dataset_path):
+    file_path = os.path.join(yale_dataset_path, file_name)
+    
+    if os.path.isdir(file_path):
+        continue  # Skip directories
+    
+    if file_name == 'Readme.txt':
+        continue  # Skip the 'Readme.txt' file
 
-#Splitting the dataset into testing and training sets
+    img = plt.imread(file_path)
+    #print(img.shape)
+    data.append(img.flatten())  # Flatten image into a 1D array
+    labels.append(extract_person_label(file_name))
+
+data = np.array(data)
+labels = np.array(labels)
+
+print("Dataset: ")
+print(data.shape)
+print("Labels: ")
+print(labels.shape)
+
+
+# Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
+# Create an instance of LabelEncoder
+label_encoder = LabelEncoder()
 
+# Fit the LabelEncoder on the training labels
+label_encoder.fit(y_train)
+
+# Convert the training and test labels to numerical format
+y_train_encoded = label_encoder.transform(y_train)
+y_test_encoded = label_encoder.transform(y_test)
+
+mlp = MLPClassifier(input_size=77760, hidden_layers=[64, 32], output_size=15)
+
+# Convert the target labels to numerical format and reshape it
+y_train_encoded = label_encoder.fit_transform(y_train)
+y_train_encoded = y_train_encoded.reshape(-1, 1)
+
+# Train the model
+mlp.fit(X_train, y_train_encoded, learning_rate = 0.1, epochs = 1000)
+
+# Obtain predictions
+y_pred_encoded = mlp.predict(X_test)
+
+# Obtain predictions
+y_pred = mlp.predict(X_test)
+
+# Compute accuracy
+accuracy = accuracy_score(y_test, y_pred)
+
+# Compute precision
+precision = precision_score(y_test, y_pred, average='micro')
+
+# Compute recall
+recall = recall_score(y_test, y_pred, average='micro')
+
+# Compute F1-score
+f1 = f1_score(y_test, y_pred, average='micro')
+
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1-score:", f1)
+
+# This implementation assumes that you have already loaded the Yale dataset and labels into the variables `dataset` and `labels`, respectively. You will need to adjust the file paths accordingly. The dataset should be in the shape `(num_samples, num_features)` and the labels should be in the shape `(num_samples,)`.
+
+# The `MLPClassifier` class provides the necessary methods for training the model (`fit`), testing the model (`test`), and tuning hyperparameters (`tune`). The `fit` method trains the model using backpropagation, the `test` method evaluates the model's accuracy on a test set, and the `tune` method performs grid search to find the best hyperparameters.
+
+# To use the `tune` method, you can specify a parameter grid containing different values for the hyperparameters you want to tune. For example:
+
+param_grid = {
+    'hidden_layers': [[32], [64, 32], [128, 64, 32]],
+    'learning_rate': [0.1, 0.01, 0.001],
+    'epochs': [100, 500, 1000]
+}
+
+# Create an instance of LabelBinarizer
+label_binarizer = LabelBinarizer()
+
+# Fit and transform the training labels to binary format
+y_train_bin = label_binarizer.fit_transform(y_train)
+
+mlp.tune(X_train, y_train_bin, param_grid)
+##Part 2
+# # Convert y_train and y_test to one-hot encoding
+# y_train = keras.utils.to_categorical(y_train, num_classes)
+# y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# # Define your MLP architecture
+# model = keras.models.Sequential([
+#     keras.layers.Dense(64, activation='relu', input_shape=(input_dim,)),
+#     keras.layers.Dense(32, activation='relu'),
+#     keras.layers.Dense(num_classes, activation='softmax')
+# ])
+
+# # Compile the model with categorical cross-entropy loss
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# # Train the model
+# model.fit(X_train, y_train, batch_size=32, epochs=10, validation_data=(X_test, y_test))
+
+# # Evaluate the model
+# loss, accuracy = model.evaluate(X_test, y_test)
+# print('Test Loss:', loss)
+# print('Test Accuracy:', accuracy)
